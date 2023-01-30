c07d10551d7c49020957fcfd067e63f03634d37c
robo <robo2020@gmail.com>
2023-01-30 22:31:15 +0200

    add camera stereo urdf

--- src/stereo_camera_demo/launch/camera.launch.py
+++ src/stereo_camera_demo/launch/camera.launch.py
@@ -5,9 +5,12 @@ from launch.actions import AppendEnvironmentVariable, DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+import xacro
+from launch_ros.actions import Node
 
 PACKAGE = "stereo_camera_demo"
 WORLD = "basic.world"
+URDF = "stereo_camera.xacro"
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -19,7 +22,8 @@ def generate_launch_description():
     arg_gazebo_verbose = DeclareLaunchArgument("verbose", default_value="true")
     world = LaunchConfiguration("world")
     arg_gazebo_world = DeclareLaunchArgument("world", default_value=WORLD)
-
+    sim_time = LaunchConfiguration("sim_time")
+    arg_sim_time = DeclareLaunchArgument("sim_time", default_value="true")
 
     resources = [os.path.join(pkg, "worlds")]
 
@@ -39,11 +43,34 @@ def generate_launch_description():
                     launch_arguments={'verbose': verbose, "world": world}.items()
              )
 
+    robot_description_path = os.path.join(pkg, "urdf", URDF)
+    doc = xacro.parse(open(robot_description_path))
+    xacro.process_doc(doc)
 
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[
+            {
+                'use_sim_time': sim_time, 
+                'robot_description': doc.toxml()
+            }
+        ]
+    )
 
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-entity", "demo", "-topic", "robot_description"],
+        output="screen",
+    )
+
+    ld.add_action(arg_sim_time)
     ld.add_action(models_env)
     ld.add_action(resource_env)
     ld.add_action(arg_gazebo_verbose)
     ld.add_action(arg_gazebo_world)
     ld.add_action(gazebo)
+    ld.add_action(robot_state_publisher)
+    ld.add_action(spawn_entity)
     return ld
